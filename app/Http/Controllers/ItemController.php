24eb55a729721a38ee3032a0bcbfc5c8c014562c
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-18 11:42:04 +0900

    add all page and show page

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $item = Item::find($id);
+        return view('items.show', ['item' => $item]);
+    }
+
+    public function index()
+    {
+        $items = Item::all();
+        return view('items.index', ['items' => $items]);
+    }
 }
