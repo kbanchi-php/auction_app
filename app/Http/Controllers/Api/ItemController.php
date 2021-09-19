e1ba6578c12e44dee61fe6a6aee60d72c66113a4
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-19 13:28:01 +0900

    update api item

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Item;
+use App\Http\Requests\ItemRequest;
 
 class ItemController extends Controller
 {
@@ -14,7 +16,8 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::all();
+        return $items;
     }
 
     /**
@@ -23,9 +26,17 @@ class ItemController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ItemRequest $request)
     {
-        //
+        $item = new Item;
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->price = $request->price;
+        $item->seller = $request->seller;
+        $item->email = $request->email;
+        $item->image_url = $request->image_url;
+        $item->save();
+        return $item;
     }
 
     /**
@@ -36,7 +47,8 @@ class ItemController extends Controller
      */
     public function show($id)
     {
-        //
+        $item = Item::find($id);
+        return $item;
     }
 
     /**
@@ -46,9 +58,17 @@ class ItemController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ItemRequest $request, $id)
     {
-        //
+        $item = Item::find($id);
+        $item->name = $request->name;
+        $item->description = $request->description;
+        $item->price = $request->price;
+        $item->seller = $request->seller;
+        $item->email = $request->email;
+        $item->image_url = $request->image_url;
+        $item->save();
+        return $item;
     }
 
     /**
@@ -59,6 +79,7 @@ class ItemController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Item::find($id);
+        $item->delete();
     }
 }
