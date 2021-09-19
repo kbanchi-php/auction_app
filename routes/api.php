003878f8202697b43034b1ce5468b57aee044ea6
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-19 11:38:06 +0900

    add api routing and controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => ['api']], function () {
+    Route::apiResource('items', App\http\Controllers\Api\ItemController::class);
 });
